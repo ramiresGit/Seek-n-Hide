1220567977d84f39a429a2b2f5c5d89af82c7500
SBay <ramiresGit@gmail.com>
2021-11-17 01:11:52 +0300

    [SK-4] Реализован алгоритм вызова питомца

--- Assets/Scripts/PetScript.cs
+++ Assets/Scripts/PetScript.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PetScript : MonoBehaviour
 {
-
+    public bool IsNeedToMove { get; set; } = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,12 +12,14 @@ public class PetScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+           
     }
 
     public void BeginDropAnim(Vector3 startPosition)
     {
         transform.position = startPosition;
+       
         GetComponent<Animator>().SetTrigger("OnDrop");
 
         Debug.Log("Begin animation");
